3429a171d739e087ebbd4b6e28de27b567af8de9
Ishai Hachlili <ishai@ishaihachlili.com>
2016-10-23 00:48:07 -0700

    Added VS add in project

--- CreateZip.ps1
+++ CreateZip.ps1
@@ -1,5 +1,5 @@
 ﻿Add-Type -Assembly "System.IO.Compression.FileSystem";
 $sourceDir =  $PSScriptRoot + "\MVVMCross.Plugin.Template";
-$outFile = $PSScriptRoot + "\MVVMCross-VSAddIn\ProjectTemplates\CSharp\MvvmCross\Root.zip";
+$outFile = $PSScriptRoot + "\MVVMCrossPlugin-VSAddIn\ProjectTemplates\CSharp\MvvmCross\Root.zip";
 Remove-Item $outFile;
 [System.IO.Compression.ZipFile]::CreateFromDirectory($sourceDir, $outFile);
