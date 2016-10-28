726013765645c5b04d53848c6e976bac2221efd7
Ishai Hachlili <ishai@ishaihachlili.com>
2016-10-28 01:08:20 -0700

    1.2.1 - fixed ios boostrap file (wrong parameter replacment)

--- MVVMCross.Plugin.Template/Nuget/IosBootstrapContent/MvvmCrossPluginBootstrap.cs.pp
+++ MVVMCross.Plugin.Template/Nuget/IosBootstrapContent/MvvmCrossPluginBootstrap.cs.pp
@@ -3,7 +3,7 @@ using MvvmCross.Platform.Plugins;
 namespace $rootnamespace$.Bootstrap
 {
     public class $ext_pluginname$PluginBootstrap
-        : MvxLoaderPluginBootstrapAction<$ext_safeprojectname$.PluginLoader, $safeprojectname$.Plugin>
+        : MvxLoaderPluginBootstrapAction<$ext_safeprojectname$.PluginLoader, $ext_safeprojectname$.iOS.Plugin>
     {
     }
 }
